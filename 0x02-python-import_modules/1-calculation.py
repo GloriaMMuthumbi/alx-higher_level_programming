4f900af2e112bd603853b6d1230d7bd65b223c33
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-11-30 08:06:43 -0800

    1. My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    a = 10
-    b = 5
     from calculator_1 import add, mul, sub, div
 
+    a = 10
+    b = 5
     add_res = add(a, b)
     mul_res = mul(a, b)
     sub_res = sub(a, b)
