cd71a43d83b54240d736c30f1bece95de87dc4ee
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-12-03 23:06:20 -0800

    6. Lists of lists = Matrix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    for char in range(len(my_string)):
-        if my_string[char] != 'c' and my_string[char] != 'C':
-            new_string += my_string[char]
+    for char in my_string:
+        if char not in ('c', 'C'):
+            new_string += char
     return(new_string)
