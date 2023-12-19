3cc1db342643f50671bb84c9daaa3e9b405a43b9
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-12-19 12:34:42 -0800

    4. Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Module documentation for Square class."""
+
+
+class Square:
+    """Square class definition"""
+
+    def __init__(self, size=0):
+        """Initialise instance of square
+
+        Args:
+            size (int): size of square.
+
+        Raises:
+            TypeError: if size is not an int
+            ValueError: if size is less than 0
+        """
+        slef.size = size
+
+    @property
+    def size(self):
+        """Getter function for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter function for size
+
+        Args:
+            value (int): value to be set as size
+
+        Raises:
+            TypeError: If value is not an int
+            ValueError: If value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            else.__size = value
+
+    def area(self):
+        """Calulates the are of square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
