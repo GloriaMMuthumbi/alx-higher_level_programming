b8d1b4d99e8b97f3e9a6ba9216127fc55bd58549
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-12-20 00:50:18 -0800

    7. Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Deifnes classes for singly-linked-list"""
 
 class Node:
     """Defines node of singly linked list."""
@@ -7,8 +8,8 @@ class Node:
         """Initializes Node instance.
 
         Args:
-            data: integer representing data of node
-            next_node: node representing the next node in list
+            data (int): integer representing data of node
+            next_node (Node): node representing the next node in list
         """
         self.data = data
         self.next_node = next_node
@@ -58,7 +59,7 @@ class SinglyLinkedList:
 
     def __init__(self):
         """Initializes an instance of singlylinked list with empty list"""
-        self.head = None
+        self.__head = None
 
     def sorted_insert(self, value):
         """Inserts a new Node into position in list
@@ -68,21 +69,25 @@ class SinglyLinkedList:
         """
         new_node = Node(value)
 
-        if self.head is None or value < self.head.data:
-            new_node.next_node = self.head
-            self.head = new_node
+        if self.__head is None:
+            new_node.next_node = None
+            self.__head = new_node
+        elif self.__head.data > value:
+            new_node.next_node == self.__head
+            self.__head = new_node
         else:
-            current = self.head
-            while current.next_node is not None and current.next_node.data < value:
+            current = self.__head
+            while (current.next_node is not None and
+                    current.next_node.data < value):
                 current = current.next_node
-                new_node.next_node = current.next_node
-                current.next_node = new_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
 
     def __str__(self):
         """String representation of linked list."""
-        result = ""
-        current = self.head
+        vals = []
+        current = self.__head
         while current is not None:
-            result += str(current.data) + "\n"
+            vals.append(str(current.data))
             current = current.next_node
-        return result
+        return '\n'.join(vals)
