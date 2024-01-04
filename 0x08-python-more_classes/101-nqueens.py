80f8d53367d35b30377450658fa7c7c6d65059cd
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-01-04 13:53:41 -0800

    10. N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -45,7 +45,7 @@ def print_solution(board):
     print(solution)
 
 
-def solve_nqueens(board, col, n):
+def solve_nqueens(board, col, n, solutions):
     """
     Solves the NQueens problem using backtracking
 
@@ -53,15 +53,21 @@ def solve_nqueens(board, col, n):
         board (list): chessboard
         col (int): current column
         n (int): size of the board
+        solutions: (list): list to store the solutions
     """
     if col == n:
-        print_solution(board)
+        solution = []
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
         return
 
     for i in range(n):
         if is_safe(board, i, col, n):
             board[i][col] = 1
-            solve_nqueens(board, col + 1, n)
+            solve_nqueens(board, col + 1, n, solutions)
             board[i][col] = 0
 
 
@@ -84,7 +90,12 @@ def nqueens(n):
         sys.exit(1)
 
     board = [[0 for _ in range(n)] for _ in range(n)]
-    solve_nqueens(board, 0, n)
+    solutions = []
+    solve_nqueens(board, 0, n, solutions)
+
+    for solution in solutions:
+        print(solution)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
