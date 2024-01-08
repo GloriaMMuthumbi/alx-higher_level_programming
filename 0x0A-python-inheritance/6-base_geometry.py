23efa0d56939e7b6ef378010e4797a302bd23da2
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-01-08 04:23:25 -0800

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+defines empty BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """ represents class basegeometry """
+    def area(self):
+        """
+        Raises:
+            Exception: area is not implemented
+        """
+        raise Exception("area() is not implemented")
