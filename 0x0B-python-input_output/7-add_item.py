21306b6bed033f27e5498116d08fc19a27680e35
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-01-09 06:51:41 -0800

    7. Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" add arguments to a python list"""
+improt sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        args = load = load_from_json_File("add_item.json")
+    except FileNotFoundError:
+        args = []
+    args.extend(sys.argv[1:])
+    save_to_json_file(args, "add_item.json")
