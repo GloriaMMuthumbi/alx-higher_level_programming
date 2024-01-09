4862cedadf71ad985d2282d45848ef4413b6a535
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-01-09 06:11:17 -0800

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def read_file(filename""):
+    """
+    Read a text file (UTF-8) and print out it's contents to stdout
+
+    Args:
+        filename (str): name of the file to be read
+    """
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            content = file.read()
+            print(content)
+    except FileNotFoundError:
+        pass
