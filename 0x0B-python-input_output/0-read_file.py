5fb7c10c8123e7c4d2e40c056160c502509b6656
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-01-09 06:16:18 -0800

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,6 +10,6 @@ def read_file(filename=""):
     try:
         with open(filename, 'r', encoding='utf-8') as file:
             content = file.read()
-            print(content)
+            print(content, end="")
     except FileNotFoundError:
         pass
