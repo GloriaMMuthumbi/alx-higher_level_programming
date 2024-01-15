5e94f816deceb157b811a61aa9555381f5e75f7c
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-01-14 23:55:36 -0800

    7. Display #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,9 +89,12 @@ class Rectangle(Base):
         return self.__width * self.__height
 
     def display(self):
-        """prints rectangle instance in the stdout using # character"""
+        """prints rectangle instance in the stdout using # character
+        considering x and y coordinates"""
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """returns string representation of rectangle instance"""
