00330ad45002dee005fa603f96dc7f6106dc2e67
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-01-14 22:26:54 -0800

    2. First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,7 +11,6 @@ class Rectangle(Base):
     defines rectangle class that inherits from Base class
     """
 
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         defines a constructor for the class Rectangle
@@ -23,9 +22,64 @@ class Rectangle(Base):
             y (int, optional): y-coordinate of rectangle. default value of 0
             id (int, optional): id of rectangle. default value of None
         """
-        super().__init__*(id)
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
+        @property
+        def width(self):
+            """getter method for width"""
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """setter method for width"""
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            if value <= 0:
+                raise ValueError("width must be a positive integer")
+            self.__width = value
+
+        @property
+        def height(self):
+            """getter method for height"""
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            """setter method for height"""
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            if value <= 0:
+                raise ValueError("height must be a positive integer")
+            self.__height = value
+
+        @property
+        def x(self):
+            """getter method for x-coordinate"""
+            return self.__x
+
+        @x.setter
+        def x(self, value):
+            """setter method for x-coordinate"""
+            if not isinstance(value, int):
+                raise TypeError("x must be an integer")
+            if value <= 0:
+                raise ValueError("x must be a none-negative integer")
+            self.__x = value
+
+        @property
+        def y(self):
+            """getter method for y-coordinate"""
+            return self.__y
+
+        @y.setter
+        def y(self, value):
+            """setter method for y-coordinate"""
+            if not isinstance(value, int):
+                raise TypeError("y must be an integer")
+            if value <= 0:
+                raise ValueError("y must be a none-negative integer")
+            self.__y = value
