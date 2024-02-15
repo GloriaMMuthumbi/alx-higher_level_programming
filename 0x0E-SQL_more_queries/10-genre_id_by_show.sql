88278fb9ad2aa0c4ee1491dcad8bb8f6dc8b10eb
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-02-15 08:40:31 -0800

    10. Genre ID by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Lists all shows contianed in hbtn_0d_tvshows that have atleast on genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+	FROM tv_show_genres
+	INNER JOIN tv_shows
+	ON tv_shows.id = tv_show_genres.show_id
+	ORDER BY tv_shows.title, tv_show_genres.genre_id;
