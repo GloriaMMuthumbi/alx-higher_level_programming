8cdf11d8582e2ddfc27ba40c8eb24373fc28be52
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-02-15 10:02:43 -0800

    17. Not my genre

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,6 +3,7 @@ SELECT DISTINCT `name`
 	FROM `tv_genres AS g
 		INNER JOIN `tv_show_genres` as s
 		ON g.`id` = s.`genre_id`
+
 		INNER JOIN `tv_shows` AS t
 		ON s.`show_id` = t.`id`
 		WHERE g.`name` NOT IN
@@ -10,7 +11,8 @@ SELECT DISTINCT `name`
 				FROM `tv_genres` AS g
 					INNER JOIN `tv_show_genres` AS s
 					ON g.`id` = s.`genre_id`
+
 					INNER JOIN `tv_shows` AS t
 					ON s.`show_id` = t.`id`
-					WHERE t.`title = "Dexter")
+					WHERE t.`title` = "Dexter")
 	ORDER BY g.`name`;
