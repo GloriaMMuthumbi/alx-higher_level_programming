ebabfa9bd1d404f3f8408c6f4384845427ae1a39
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-02-15 10:22:20 -0800

    20. Best genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -4,7 +4,7 @@ SELECT `name`, SUM(`rate`) AS `rating`
 		INNER JOIN `tv_show_genres` AS s
 		ON s.`genre_id` = g.`id`
 
-		INNER JOIN `tv_show_Ratings` AS r
+		INNER JOIN `tv_show_ratings` AS r
 		ON r.`show_id` = s.`show_id`
 	GROUP BY `name`
 	ORDER BY `rating` DESC;
