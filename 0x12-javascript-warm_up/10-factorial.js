b17fae827445b4f59bd4900947d270f7a7e31ca8
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-03-11 15:58:16 -0700

    10. Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-function factorial(n) {
+function factorial (n) {
   if (isNaN(n) || n < 0) {
     return 1;
-  } else if (n ===0 || n === 1) {
+  } else if (n === 0 || n === 1) {
     return 1;
   } else {
     return n * factorial(n - 1);
