955f29adfa914d92db928f45913fa2ac9f2e1410
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-03-12 07:12:46 -0700

    6. Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,7 +2,8 @@
 module.exports = class Square extends require('./5-square') {
   charPrint (c) {
     if (c === undefined) {
-      this.print(); }
+      this.print();
+    }
     else {
       for (let i = 0; i < this.height; i++) {
         console.log(c.repeat(this.width));
