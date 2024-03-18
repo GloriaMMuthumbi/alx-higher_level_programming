08d0cf599f2fb076d361f1dd9888621457729212
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-03-18 05:23:27 -0700

    1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Lists all states with name starting with uppercase N from database
+Usage: ./1-filter_states.py <mysql username> \
+                            <mysql password>
+                            <databse name>
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+            user=sys.argv[1], user=sys.argv[2], database=sys.argv[3])
+    mycursor = db.cursor()
+    myscursor.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in mycursor.fetchall() if state[1][0] == "N"]
