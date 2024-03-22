9beacbb7dae6cd64ee415202ab5555f6656a85f4
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-03-22 13:22:32 -0700

    6. First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,18 @@
--- Create database hbtn_0e_6_usa;
-CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
-USE hbtn_0e_6_usa;
-SHOW CREATE TABLE states;
+#!/usr/bin/python3
+"""Defines model state containing class definition
+of State and instance Base = declarative_base()
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    class State that inherits from Base
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
