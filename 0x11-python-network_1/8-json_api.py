57508d2bf7e46af78e38dbe3767beb18046ea438
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-04-02 06:59:59 -0700

    8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,11 +9,11 @@ import requests
 
 if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    pl = {"q": letter)
+    pl = {"q": letter}
 
     reqs = requests.post("http://0.0.0.0:5000/search_user", data=pl)
     try:
-        reponse = reqs.json()
+        response = reqs.json()
         if response == {}:
             print("No result")
         else:
