4f5807a7012fe65a185ede1348f00b800cb37eb6
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-04-23 06:44:56 -0700

    3. Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,10 +2,10 @@
 const request = require('request');
 const { argv } = require('process');
 
-const BaseUrl = 'https://swapi-api.alx-tools.com/api/films/:id';
-request(BaseUrl + '/films/' + argv[2], (error, response, body) => {
+const BaseUrl = 'https://swapi-api.alx-tools.com/api/films/';
+request(BaseUrl + argv[2], (error, response, body) => {
 	if (error) {
 		console.error(error);
 	}
-	console.log(JSON.parse(body).tile);
+	console.log(JSON.parse(body).title);
 });
